8ffeecef452148a97b7406d23ae2b4ac67cf1595
Aleksej Shvajka <shvaikalesh@gmail.com>
2015-08-08 17:18:56 +0300

    Object.setPrototypeOf polyfill
    for Chrome < 34 and Firefox < 31

---
+++ set-prototype-of.js
@@ -0,0 +1,53 @@
+(function()
+{
+    "use strict"
+
+    if (typeof Object.setPrototypeOf == "function")
+        return Function.prototype
+
+    var PROTO = "__proto__"
+    if (PROTO in { })
+    {
+        return function(global)
+        {
+            var _Object_ = global.Object
+
+            var $SetPrototypeOf$ = _Object_
+                .getOwnPropertyDescriptor(_Object_.prototype, PROTO)
+                .set
+
+            Object.defineProperty(_Object_, SET_PROTO,
+            {
+                value: function(target, prototype)
+                {
+                    if (null == target)
+                        throw new global.TypeError()
+
+                    if (!$IsObjectOrNull$(prototype))
+                        throw new global.TypeError()
+
+                    if (!$IsObjectOrNull$(target))
+                        return target
+
+                    $SetPrototypeOf$.call(target, prototype)
+
+                    return target
+                },
+                writable: true,
+                configurable: true
+            })
+        }
+    }
+
+    function $IsObjectOrNull$(value)
+    {
+        switch (typeof value)
+        {
+            case "object":
+            case "function":
+                return true
+        }
+
+        return false
+    }
+})()(this)
