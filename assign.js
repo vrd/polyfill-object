8778f19a34f2b336263f46cd86af95782300f623
Aleksej Shvajka <shvaikalesh@gmail.com>
2015-08-08 16:06:03 +0300

    Object.assign polyfill
    for Chrome < 45 and Firefox < 34

---
+++ assign.js
@@ -0,0 +1,54 @@
+(function()
+{
+    "use strict"
+
+    var ASSIGN = "assign"
+    if (ASSIGN in Object) return Function.prototype
+
+    return function(global)
+    {
+        var _Object_ = global.Object
+
+        var $OwnStrings$ = _Object_.getOwnPropertyNames
+        var $OwnSymbols$ = _Object_.getOwnPropertySymbols
+
+        var $IsEnumerable$ = _Object_.prototype.propertyIsEnumerable
+
+        ///
+
+        _Object_.defineProperty(_Object_, ASSIGN,
+        {
+            value: function(target, sources)
+            {
+                if (null == target) throw new global.TypeError()
+
+                var to = _Object_(target)
+
+                if (arguments.length == 1) return to
+
+                for (var index = 1; index < arguments.length; ++index)
+                {
+                    var source = arguments[index]
+                    if (source == null) continue
+
+                    var from = _Object_(source)
+                    var keys = $OwnStrings$(from).concat(
+                        typeof $OwnSymbols$ == "function"
+                            && $OwnSymbols$(from)
+                            || [ ]
+                    )
+
+                    keys.forEach(function(key)
+                    {
+                        if ($IsEnumerable$.call(from, key))
+                            to[key] = from[key]
+                    })
+                }
+
+                return to
+            },
+            writable: true,
+            configurable: true
+        })
+    }
+})()(this)
