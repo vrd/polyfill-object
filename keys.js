19398fa1f6315f47bff7c90250c168231b564a14
Aleksej Shvajka <shvaikalesh@gmail.com>
2015-08-09 21:11:53 +0300

    Object.keys polyfill
    for IE8

---
+++ keys.js
@@ -0,0 +1,30 @@
+(function()
+{
+    "use strict"
+
+    if (typeof Object.keys == "function")
+        return Function.prototype
+
+    return function(global)
+    {
+        var _Object_ = global.Object
+
+        var $GetOwn$ = _Object_.getOwnPropertyNames
+          , $IsEnumerable$ = _Object_.prototype.propertyIsEnumerable
+
+        ///
+
+        _Object_.defineProperty(_Object_, "keys",
+        {
+            value: function(target)
+            {
+                return $GetOwn$(target).filter(function(name)
+                {
+                    return $IsEnumerable$.call(target, name)
+                })
+            },
+            writable: true,
+            configurable: true
+        })
+    }
+})()(this)
