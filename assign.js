d38ef4b550735ea0d0a20015588d3bbd19282a0d
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-02-03 16:44:06 +0200

    added ownKeys helper
    changed code style

--- assign.js
+++ assign.js
@@ -5,44 +5,47 @@
     if (typeof Object.assign == "function")
         return module.exports = Object
 
-    var $OwnNames$ = Object.getOwnPropertyNames
-      , $OwnSymbols$ = Object.getOwnPropertySymbols
-      , $IsEnumerable$ = { }.propertyIsEnumerable
+    var isEnumerable = { }.propertyIsEnumerable
+      , ownStrings = Object.getOwnPropertyNames
+      , ownSymbols = Object.getOwnPropertySymbols
+      , ownKeys = ownStrings
+
+    if (typeof ownSymbols == "function")
+    {
+        ownKeys = function(object)
+        {
+            return ownStrings(object)
+                .concat(ownSymbols(object))
+        }
+    }
 
     function polyfill(global)
     {
-        function assign(target, sources)
-        {
-            if (target == null) throw new global.TypeError
-
-            var to = Object(target)
-
-            for (var index = 1; index < arguments.length ;)
-            {
-                var source = arguments[index++]
-                if (source == null) continue
-
-                var from = Object(source)
-                var keys = $OwnNames$(from).concat
-                (
-                    typeof $OwnSymbols$ == "function"
-                        && $OwnSymbols$(from)
-                        || [ ]
-                )
-
-                keys.forEach(function(key)
-                {
-                    if ($IsEnumerable$.call(from, key))
-                        to[key] = from[key]
-                })
-            }
-
-            return to
-        }
-
         Object.defineProperty(global.Object, "assign",
         {
-            value: assign,
+            value: function assign(target, sources)
+            {
+                if (target == null) throw new global.TypeError
+
+                var to = Object(target)
+
+                for (var index = 1; index < arguments.length ;)
+                {
+                    var source = arguments[index++]
+                    if (source == null) continue
+
+                    var from = Object(source)
+                    var keys = ownKeys(from)
+
+                    keys.forEach(function(key)
+                    {
+                        if (isEnumerable.call(from, key))
+                            to[key] = from[key]
+                    })
+                }
+
+                return to
+            },
             writable: true,
             configurable: true
         })
