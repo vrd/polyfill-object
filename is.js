690855f56ee9c7ca860ee272041a40c8ef623e16
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-02-03 16:46:43 +0200

    replaced declaration with definition

--- is.js
+++ is.js
@@ -7,20 +7,18 @@
 
     function polyfill(global)
     {
-        function is(a, b)
-        {
-            if (a === 0 && b === 0)
-                return 1 / a === 1 / b
-
-            if (a !== a && b !== b)
-                return true
-
-            return a === b
-        }
-
         Object.defineProperty(global.Object, "is",
         {
-            value: is,
+            value: function is(a, b)
+            {
+                if (a === 0 && b === 0)
+                    return 1 / a === 1 / b
+
+                if (a !== a && b !== b)
+                    return true
+
+                return a === b
+            },
             writable: true,
             configurable: true
         })
