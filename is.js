72fa9361b498a39c3430c10b1d9b6b8fe92b5e23
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-06-01 04:18:43 +0300

    prefer abstract equality

--- is.js
+++ is.js
@@ -2,18 +2,14 @@
 {
     "use strict"
 
-    if (typeof Object.is == "function")
-        return
+    if (typeof Object.is == "function") return
 
     Object.defineProperty(Object, "is",
     {
         value: function is(a, b)
         {
-            if (a === 0 && b === 0)
-                return 1 / b === 1 / a
-
-            if (a !== a && b !== b)
-                return true
+            if (a != a && b != b) return true
+            if (a === 0 && b === 0) return 1 / a == 1 / b
 
             return a === b
         },
